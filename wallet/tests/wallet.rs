2dcf7ad03c72b616456fe92f7e6c9491c6f9721c
Luis Schwab <luisschwab@protonmail.com>
2025-04-21 23:35:36 -0300

    test: add `test_legacy_create_tx_absolute_high_fee()`

--- wallet/tests/wallet.rs
+++ wallet/tests/wallet.rs
@@ -1057,6 +1057,19 @@ fn test_create_tx_absolute_high_fee() {
 }
 
 #[test]
+#[should_panic(expected = "InsufficientFunds")]
+fn test_legacy_create_tx_absolute_high_fee() {
+    let (mut wallet, _) = get_funded_wallet_single(get_test_pkh());
+    let addr = wallet.next_unused_address(KeychainKind::External);
+    let mut builder = wallet.build_tx();
+    builder
+        .drain_to(addr.script_pubkey())
+        .drain_wallet()
+        .fee_absolute(Amount::from_sat(60_000));
+    let _ = builder.finish().unwrap();
+}
+
+#[test]
 fn test_create_tx_add_change() {
     use bdk_wallet::tx_builder::TxOrdering;
     let seed = [0; 32];
