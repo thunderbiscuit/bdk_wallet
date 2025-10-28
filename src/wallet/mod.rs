8b3062fa9b3d296767f74174f69d544a4645164e
codingp110 <codingp110@gmail.com>
2025-10-28 16:38:33 +0530

    feat: add method to get latest checkpoint.

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -626,6 +626,11 @@ where
     fn stage(&mut self, changeset: impl Into<ChangeSet<K>>) {
         self.stage.merge(changeset.into());
     }
+
+    /// Returns the latest checkpoint.
+    pub fn latest_checkpoint(&self) -> CheckPoint {
+        self.chain.tip()
+    }
 }
 
 // impl Wallet {
